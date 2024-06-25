8b092d3aa78f34029d974989d7d0d096ca485e25
rukije <rukije.morina@harrisia.com>
2024-06-25 10:54:53 +0200

    imporoving the buttons

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,12 +5,22 @@ import PrimaryButton from '../components/PrimaryButtons';
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput style={styles.numberInput} maxLength={2} 
-     keyboardType="number-pad"
-     autoCapitalize="none" 
-     autoCorrect={false}/>
-        <PrimaryButton>Reset</PrimaryButton>
-        <PrimaryButton>Confirm</PrimaryButton>
+      <TextInput 
+         style={styles.numberInput} 
+         maxLength={2} 
+         keyboardType="number-pad"
+         autoCapitalize="none" 
+         autoCorrect={false} 
+         />
+         <View style={styles.buttonsContainer}>
+          <View style={styles.buttonContainer}>           
+            <PrimaryButton>Reset</PrimaryButton>
+          </View>
+          <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+
+          </View>
+         </View>
     </View>
   );
 }
@@ -19,6 +29,8 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent:'center',
+    alignItems:'center',
     marginTop:100,
     marginHorizontal:24,
     padding:16,
@@ -40,5 +52,11 @@ const styles = StyleSheet.create({
     marginVertical:8,
     fontWeight:'bold',
     textAlign:'center'
+  },
+  buttonsContainer:{
+    flexDirection:'row',
+  },
+  buttonContainer:{
+    flex:1,
   }
 })
